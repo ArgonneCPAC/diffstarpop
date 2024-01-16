93456337b123b61825a8535e34f8832adf808fcd
Andrew Hearin <ahearin@anl.gov>
2024-01-16 17:53:01 -0600

    Add docstrings

--- diffstarpop/mc_diffstarpop.py
+++ diffstarpop/mc_diffstarpop.py
@@ -1,4 +1,4 @@
-"""
+"""This module implements kernels for Monte Carlo generating Diffstar SFHs
 """
 from collections import OrderedDict
 
@@ -21,6 +21,49 @@ from .kernels.legacy_wrapper import mc_diffstar_u_params_singlegal_kernel
 def mc_diffstar_sfh_singlegal(
     diffstarpop_params, mah_params, p50, ran_key, tarr, lgt0=LGT0, fb=FB
 ):
+    """Monte Carlo realization of a single point in Diffstar parameter space,
+    along with the computation of SFH for this point.
+
+    Parameters
+    ----------
+    diffstarpop_params : namedtuple
+        See defaults.DEFAULT_DIFFSTARPOP_PARAMS for an example
+
+    mah_params : namedtuple, length 4
+        mah_params is a tuple of floats
+        DiffmahParams = logmp, logtc, early_index, late_index
+
+    p50 : float
+        Prob(<t_50% | logm0), the CDF of the distribution of halo
+        formation times t_50% conditioned on mass logm0
+
+    ran_key : jax.random.PRNGKey
+        Single instance of a jax randum seed
+
+    tarr : ndarray, shape (nt, )
+
+    lgt0 : float, optional
+        Base-10 log of the z=0 age of the Universe in Gyr
+        Default is set in diffstar.defaults
+        This variable should be self-consistently set with cosmology
+
+    fb : float, optional
+        Cosmic baryon fraction Ob0/Om0
+        Default is set in diffstar.defaults
+        This variable should be self-consistently set with cosmology
+
+    Returns
+    -------
+    diffstar_params : namedtuple
+        DiffstarParams = ms_params, q_params
+            ms_params and q_params are tuples of floats
+            diffstar_params.ms_params = lgmcrit, lgy_at_mcrit, indx_lo, indx_hi, tau_dep
+            diffstar_params.q_params = lg_qt, qlglgdt, lg_drop, lg_rejuv
+
+    sfh : ndarray, shape (nt, )
+        Star formation rate in units of Msun/yr
+
+    """
     diffstar_params = mc_diffstar_params_singlegal(
         diffstarpop_params, mah_params, p50, ran_key
     )
@@ -97,6 +140,49 @@ def mc_diffstar_params_galpop(diffstarpop_params, mah_params, p50, ran_key):
 def mc_diffstar_sfh_galpop(
     diffstarpop_params, mah_params, p50, ran_key, tarr, lgt0=LGT0, fb=FB
 ):
+    """Monte Carlo realization of a single point in Diffstar parameter space,
+    along with the computation of SFH for this point.
+
+    Parameters
+    ----------
+    diffstarpop_params : namedtuple
+        See defaults.DEFAULT_DIFFSTARPOP_PARAMS for an example
+
+    mah_params : namedtuple, length 4
+        mah_params is a tuple of ndarrays of shape (ngals, )
+        DiffmahParams = logmp, logtc, early_index, late_index
+
+    p50 : ndarray of shape (ngals, )
+        Prob(<t_50% | logm0), the CDF of the distribution of halo
+        formation times t_50% conditioned on mass logm0
+
+    ran_key : jax.random.PRNGKey
+        Single instance of a jax randum seed
+
+    tarr : ndarray, shape (nt, )
+
+    lgt0 : float, optional
+        Base-10 log of the z=0 age of the Universe in Gyr
+        Default is set in diffstar.defaults
+        This variable should be self-consistently set with cosmology
+
+    fb : float, optional
+        Cosmic baryon fraction Ob0/Om0
+        Default is set in diffstar.defaults
+        This variable should be self-consistently set with cosmology
+
+    Returns
+    -------
+    diffstar_params : namedtuple
+        DiffstarParams = ms_params, q_params
+            ms_params and q_params are tuples of ndarrays of shape (ngals, )
+            diffstar_params.ms_params = lgmcrit, lgy_at_mcrit, indx_lo, indx_hi, tau_dep
+            diffstar_params.q_params = lg_qt, qlglgdt, lg_drop, lg_rejuv
+
+    sfh : ndarray, shape (ngals, nt)
+        Star formation rate in units of Msun/yr
+
+    """
     diffstar_params = mc_diffstar_params_galpop(
         diffstarpop_params, mah_params, p50, ran_key
     )
