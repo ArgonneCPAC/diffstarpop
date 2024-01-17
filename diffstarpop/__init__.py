766931c3294e93b0692acd8b94f535b06878430e
Andrew Hearin <ahearin@anl.gov>
2024-01-17 10:34:13 -0600

    Add DEFAULT_DIFFSTARPOP_PARAMS to top-level import

--- diffstarpop/__init__.py
+++ diffstarpop/__init__.py
@@ -1,6 +1,7 @@
 """
 """
 # flake8: noqa
+from .defaults import DEFAULT_DIFFSTARPOP_PARAMS, DiffstarPopParams
 from .mc_diffstarpop import (
     mc_diffstar_params_galpop,
     mc_diffstar_params_singlegal,
