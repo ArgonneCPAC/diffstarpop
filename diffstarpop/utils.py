f354510acec087b93a0bf0250cabd060c1438f71
Andrew Hearin <ahearin@anl.gov>
2024-06-19 09:01:39 -0500

    Add correlation_from_covariance utils

--- diffstarpop/utils.py
+++ diffstarpop/utils.py
@@ -125,3 +125,46 @@ def get_t50_p50(t_table, histories, threshold, logmpeak, window_length=101):
     t50 = jax_np_interp_vmap(hist_val_at_threshold, histories, t_table, indices)
     p50 = sliding_conditional_percentile(logmpeak, t50, window_length=window_length)
     return t50, p50
+
+
+@jjit
+def correlation_from_covariance(cov):
+    """Correlation matrix from covariance matrix
+
+    Parameters
+    ----------
+    cov : array, shape (n, n)
+
+    Returns
+    -------
+    corr : array, shape (n, n)
+
+    """
+    v = jnp.sqrt(jnp.diag(cov))
+    outer_v = jnp.outer(v, v)
+    corr = cov / outer_v
+    msk = cov == 0
+    corr = jnp.where(msk, 0.0, corr)
+    return corr
+
+
+@jjit
+def covariance_from_correlation(corr, evals):
+    """Covariance matrix from correlation matrix
+
+    Parameters
+    ----------
+    corr : array, shape (n, n)
+
+    evals : array, shape (n, )
+        Array of eigenvalues, e.g. (σ_1, σ_2, ..., σ_n)
+        Note that np.diag(cov) = evals**2
+
+    Returns
+    -------
+    cov : array, shape (n, n)
+
+    """
+    D = jnp.diag(evals)
+    cov = jnp.dot(jnp.dot(D, corr), D)
+    return cov
