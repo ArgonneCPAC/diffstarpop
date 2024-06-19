2a66e2b14ebe5b98a6f6afe39ac793944026912b
Andrew Hearin <ahearin@anl.gov>
2024-06-19 12:24:06 -0500

    Finish unit testing of new qseq module

--- diffstarpop/kernels/qseq_massonly_block_cov.py
+++ diffstarpop/kernels/qseq_massonly_block_cov.py
@@ -23,18 +23,18 @@ SFH_PDF_QUENCH_MU_PDICT = OrderedDict(
     mean_ulgm_quench_yhi=12.080,
     mean_ulgy_quench_ylo=-0.481,
     mean_ulgy_quench_yhi=-0.223,
-    mean_ul_quench_ylo=-1.274,
-    mean_ul_quench_yhi=1.766,
-    mean_utau_quench_ylo=55.480,
-    mean_utau_quench_yhi=-66.540,
-    mean_uqt_quench_ylo=1.744,
-    mean_uqt_quench_yhi=0.042,
-    mean_uqs_quench_ylo=-2.979,
-    mean_uqs_quench_yhi=3.520,
-    mean_udrop_quench_ylo=-0.508,
-    mean_udrop_quench_yhi=-3.785,
-    mean_urej_quench_ylo=2.139,
-    mean_urej_quench_yhi=-3.043,
+    mean_ul_quench_ylo=-0.75,
+    mean_ul_quench_yhi=0.75,
+    mean_utau_quench_ylo=20.0,
+    mean_utau_quench_yhi=-10.0,
+    mean_uqt_quench_ylo=1.3,
+    mean_uqt_quench_yhi=0.5,
+    mean_uqs_quench_ylo=-0.5,
+    mean_uqs_quench_yhi=0.5,
+    mean_udrop_quench_ylo=-2.0,
+    mean_udrop_quench_yhi=-2.0,
+    mean_urej_quench_ylo=0.5,
+    mean_urej_quench_yhi=-2.5,
 )
 SFH_PDF_QUENCH_MU_BOUNDS_PDICT = OrderedDict(
     mean_lgmhalo_x0=(11.5, 13.5),
@@ -42,18 +42,18 @@ SFH_PDF_QUENCH_MU_BOUNDS_PDICT = OrderedDict(
     mean_ulgm_quench_yhi=(11.5, 13.0),
     mean_ulgy_quench_ylo=(-1.0, 0.0),
     mean_ulgy_quench_yhi=(-1.0, 0.0),
-    mean_ul_quench_ylo=(-0.5, 0.5),
-    mean_ul_quench_yhi=(-0.5, 0.5),
-    mean_utau_quench_ylo=(-5.0, 20.0),
-    mean_utau_quench_yhi=(-5.0, 20.0),
-    mean_uqt_quench_ylo=(2.0, 0.0),
-    mean_uqt_quench_yhi=(2.0, 0.0),
-    mean_uqs_quench_ylo=(-2.0, 2.0),
-    mean_uqs_quench_yhi=(-2.0, 2.0),
-    mean_udrop_quench_ylo=(-3.0, -1.0),
-    mean_udrop_quench_yhi=(-3.0, -1.0),
-    mean_urej_quench_ylo=(-3.0, 1.0),
-    mean_urej_quench_yhi=(-3.0, 1.0),
+    mean_ul_quench_ylo=(-1.0, 1.0),
+    mean_ul_quench_yhi=(-1.0, 1.0),
+    mean_utau_quench_ylo=(-25.0, 50.0),
+    mean_utau_quench_yhi=(-25.0, 50.0),
+    mean_uqt_quench_ylo=(0.0, 2.0),
+    mean_uqt_quench_yhi=(0.0, 2.0),
+    mean_uqs_quench_ylo=(-5.0, 2.0),
+    mean_uqs_quench_yhi=(-5.0, 2.0),
+    mean_udrop_quench_ylo=(-3.0, 2.0),
+    mean_udrop_quench_yhi=(-3.0, 2.0),
+    mean_urej_quench_ylo=(-10.0, 2.0),
+    mean_urej_quench_yhi=(-10.0, 2.0),
 )
 
 SFH_PDF_QUENCH_COV_MS_BLOCK_PDICT = OrderedDict(
@@ -83,8 +83,8 @@ SFH_PDF_QUENCH_COV_MS_BLOCK_BOUNDS_PDICT = OrderedDict(
     std_ulgm_quench_yhi=(0.1, 1.0),
     std_ulgy_quench_ylo=(0.1, 1.0),
     std_ulgy_quench_yhi=(0.1, 1.0),
-    std_ul_quench_ylo=(0.5, 3.0),
-    std_ul_quench_yhi=(0.5, 3.0),
+    std_ul_quench_ylo=(0.25, 3.0),
+    std_ul_quench_yhi=(0.25, 3.0),
     std_utau_quench_ylo=(1.0, 8.0),
     std_utau_quench_yhi=(1.0, 8.0),
     rho_ulgy_ulgm_quench_ylo=RHO_BOUNDS,
@@ -124,14 +124,14 @@ SFH_PDF_QUENCH_COV_Q_BLOCK_PDICT = OrderedDict(
     rho_urej_udrop_quench_yhi=0.0,
 )
 SFH_PDF_QUENCH_COV_Q_BLOCK_BOUNDS_PDICT = OrderedDict(
-    std_uqt_quench_ylo=(0.1, 0.5),
-    std_uqt_quench_yhi=(0.1, 0.5),
-    std_uqs_quench_ylo=(0.1, 1.0),
-    std_uqs_quench_yhi=(0.1, 1.0),
-    std_udrop_quench_ylo=(0.1, 1.0),
-    std_udrop_quench_yhi=(0.1, 1.0),
-    std_urej_quench_ylo=(0.1, 1.0),
-    std_urej_quench_yhi=(0.1, 1.0),
+    std_uqt_quench_ylo=(0.01, 0.5),
+    std_uqt_quench_yhi=(0.01, 0.5),
+    std_uqs_quench_ylo=(0.01, 1.0),
+    std_uqs_quench_yhi=(0.01, 1.0),
+    std_udrop_quench_ylo=(0.01, 1.0),
+    std_udrop_quench_yhi=(0.01, 1.0),
+    std_urej_quench_ylo=(0.01, 1.0),
+    std_urej_quench_yhi=(0.01, 1.0),
     rho_uqs_uqt_quench_ylo=RHO_BOUNDS,
     rho_uqs_uqt_quench_yhi=RHO_BOUNDS,
     rho_udrop_uqt_quench_ylo=RHO_BOUNDS,
@@ -145,107 +145,28 @@ SFH_PDF_QUENCH_COV_Q_BLOCK_BOUNDS_PDICT = OrderedDict(
     rho_urej_udrop_quench_ylo=RHO_BOUNDS,
     rho_urej_udrop_quench_yhi=RHO_BOUNDS,
 )
-DEFAULT_SFH_PDF_QUENCH_MS_BLOCK_PDICT = OrderedDict(
-    mean_ulgm_quench_ylo=11.540,
-    mean_ulgm_quench_yhi=12.080,
-    mean_ulgy_quench_ylo=0.481,
-    mean_ulgy_quench_yhi=-0.223,
-    mean_ul_quench_ylo=-1.274,
-    mean_ul_quench_yhi=1.766,
-    mean_utau_quench_ylo=55.480,
-    mean_utau_quench_yhi=-66.540,
-    chol_ulgm_ulgm_quench_ylo=-1.645,
-    chol_ulgm_ulgm_quench_yhi=0.010,
-    chol_ulgy_ulgy_quench_ylo=-1.125,
-    chol_ulgy_ulgy_quench_yhi=-0.530,
-    chol_ul_ul_quench_ylo=-0.701,
-    chol_ul_ul_quench_yhi=0.544,
-    chol_utau_utau_quench_ylo=0.833,
-    chol_utau_utau_quench_yhi=1.100,
-    chol_ulgy_ulgm_quench_ylo=-0.809,
-    chol_ulgy_ulgm_quench_yhi=-1.790,
-    chol_ul_ulgm_quench_ylo=0.277,
-    chol_ul_ulgm_quench_yhi=0.357,
-    chol_ul_ulgy_quench_ylo=0.152,
-    chol_ul_ulgy_quench_yhi=0.068,
-    chol_utau_ulgm_quench_ylo=-1.214,
-    chol_utau_ulgm_quench_yhi=-0.822,
-    chol_utau_ulgy_quench_ylo=0.115,
-    chol_utau_ulgy_quench_yhi=0.204,
-    chol_utau_ul_quench_ylo=-0.566,
-    chol_utau_ul_quench_yhi=-0.848,
-)
-DEFAULT_SFH_PDF_QUENCH_Q_BLOCK_PDICT = OrderedDict(
-    mean_uqt_quench_ylo=1.744,
-    mean_uqt_quench_yhi=0.042,
-    mean_uqs_quench_ylo=-2.979,
-    mean_uqs_quench_yhi=3.520,
-    mean_udrop_quench_ylo=-0.508,
-    mean_udrop_quench_yhi=-3.785,
-    mean_urej_quench_ylo=2.139,
-    mean_urej_quench_yhi=-3.043,
-    chol_uqt_uqt_quench_ylo=-1.001,
-    chol_uqt_uqt_quench_yhi=-1.228,
-    chol_uqs_uqs_quench_ylo=-0.814,
-    chol_uqs_uqs_quench_yhi=-0.560,
-    chol_udrop_udrop_quench_ylo=-0.612,
-    chol_udrop_udrop_quench_yhi=-0.824,
-    chol_urej_urej_quench_ylo=0.560,
-    chol_urej_urej_quench_yhi=-1.103,
-    chol_uqs_uqt_quench_ylo=-0.395,
-    chol_uqs_uqt_quench_yhi=-0.508,
-    chol_udrop_uqt_quench_ylo=2.323,
-    chol_udrop_uqt_quench_yhi=3.009,
-    chol_udrop_uqs_quench_ylo=1.021,
-    chol_udrop_uqs_quench_yhi=-0.074,
-    chol_urej_uqt_quench_ylo=-0.508,
-    chol_urej_uqt_quench_yhi=0.758,
-    chol_urej_uqs_quench_ylo=1.561,
-    chol_urej_uqs_quench_yhi=2.030,
-    chol_urej_udrop_quench_ylo=-1.445,
-    chol_urej_udrop_quench_yhi=-2.245,
-)
-DEFAULT_SFH_PDF_QUENCH_BLOCK_PDICT = OrderedDict(
+SFH_PDF_FRAC_QUENCH_PDICT = OrderedDict(
     frac_quench_x0=11.860,
     frac_quench_k=4.5,
     frac_quench_ylo=0.05,
     frac_quench_yhi=0.95,
 )
-DEFAULT_SFH_PDF_FRAC_QUENCH_PDICT = OrderedDict(
-    frac_quench_x0=11.860,
-    frac_quench_k=4.5,
-    frac_quench_ylo=0.05,
-    frac_quench_yhi=0.95,
-)
-DEFAULT_SFH_PDF_FRAC_QUENCH_BOUNDS_PDICT = OrderedDict(
+SFH_PDF_FRAC_QUENCH_BOUNDS_PDICT = OrderedDict(
     frac_quench_x0=(11.0, 13.0),
     frac_quench_k=(1.0, 5.0),
     frac_quench_ylo=(0.0, 0.5),
     frac_quench_yhi=(0.25, 1.0),
 )
 
-DEFAULT_SFH_PDF_QUENCH_BLOCK_PDICT.update(DEFAULT_SFH_PDF_QUENCH_MS_BLOCK_PDICT)
-DEFAULT_SFH_PDF_QUENCH_BLOCK_PDICT.update(DEFAULT_SFH_PDF_QUENCH_Q_BLOCK_PDICT)
-
-SFH_PDF_QUENCH_PDICT = DEFAULT_SFH_PDF_FRAC_QUENCH_PDICT.copy()
+SFH_PDF_QUENCH_PDICT = SFH_PDF_FRAC_QUENCH_PDICT.copy()
 SFH_PDF_QUENCH_PDICT.update(SFH_PDF_QUENCH_MU_PDICT)
 SFH_PDF_QUENCH_PDICT.update(SFH_PDF_QUENCH_COV_MS_BLOCK_PDICT)
 SFH_PDF_QUENCH_PDICT.update(SFH_PDF_QUENCH_COV_Q_BLOCK_PDICT)
 
-SFH_PDF_QUENCH_BOUNDS_PDICT = DEFAULT_SFH_PDF_FRAC_QUENCH_BOUNDS_PDICT.copy()
+SFH_PDF_QUENCH_BOUNDS_PDICT = SFH_PDF_FRAC_QUENCH_BOUNDS_PDICT.copy()
 SFH_PDF_QUENCH_BOUNDS_PDICT.update(SFH_PDF_QUENCH_MU_BOUNDS_PDICT)
 SFH_PDF_QUENCH_BOUNDS_PDICT.update(SFH_PDF_QUENCH_COV_MS_BLOCK_BOUNDS_PDICT)
 SFH_PDF_QUENCH_BOUNDS_PDICT.update(SFH_PDF_QUENCH_COV_Q_BLOCK_BOUNDS_PDICT)
-
-QseqMassOnlyBlockParams = namedtuple(
-    "QseqMassOnlyBlockParams", list(DEFAULT_SFH_PDF_QUENCH_BLOCK_PDICT.keys())
-)
-DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS = QseqMassOnlyBlockParams(
-    **DEFAULT_SFH_PDF_QUENCH_BLOCK_PDICT
-)
-
-_UPNAMES = ["u_" + key for key in DEFAULT_SFH_PDF_QUENCH_BLOCK_PDICT.keys()]
-QseqMassOnlyBlockUParams = namedtuple("QseqMassOnlyBlockUParams", _UPNAMES)
 
 
 QseqParams = namedtuple("QseqParams", list(SFH_PDF_QUENCH_PDICT.keys()))
@@ -254,76 +175,6 @@ SFH_PDF_QUENCH_PBOUNDS = QseqParams(**SFH_PDF_QUENCH_BOUNDS_PDICT)
 
 _UPNAMES = ["u_" + key for key in QseqParams._fields]
 QseqUParams = namedtuple("QseqUParams", _UPNAMES)
-
-FRAC_Q_BOUNDS_PDICT = OrderedDict(
-    frac_quench_x0=(10.0, 15.0),
-    frac_quench_k=(1.0, 5.0),
-    frac_quench_ylo=(0.0, 1.0),
-    frac_quench_yhi=(0.0, 1.0),
-)
-FRAC_Q_PNAMES = list(FRAC_Q_BOUNDS_PDICT.keys())
-FracQParams = namedtuple("FracQParams", FRAC_Q_PNAMES)
-_FRAC_Q_UPNAMES = ["u_" + key for key in FRAC_Q_PNAMES]
-FracQUParams = namedtuple("FracQUParams", _FRAC_Q_UPNAMES)
-
-FRAC_Q_BOUNDS = FracQParams(**FRAC_Q_BOUNDS_PDICT)
-
-
-@jjit
-def _get_qseq_means_and_covs_scalar(params, lgm):
-    _res = _get_mean_u_params_qseq(params, lgm)
-    mu_ms = jnp.array(_res[:4])
-    mu_q = jnp.array(_res[4:])
-    cov_ms = _get_cov_qseq_ms_block(params, lgm)
-    cov_q = _get_cov_qseq_q_block(params, lgm)
-    frac_q = _frac_quench_vs_lgm0(params, lgm)
-    return mu_ms, cov_ms, mu_q, cov_q, frac_q
-
-
-_M = (None, 0)
-_get_qseq_means_and_covs_vmap = jjit(vmap(_get_qseq_means_and_covs_scalar, in_axes=_M))
-
-
-@jjit
-def _fun(x, ymin, ymax):
-    return _sigmoid(x, LGM_X0, LGM_K, ymin, ymax)
-
-
-@jjit
-def _fun_Mcrit(x, ymin, ymax):
-    return _sigmoid(x, 12.0, LGMCRIT_K, ymin, ymax)
-
-
-@jjit
-def _fun_chol_diag(x, ymin, ymax):
-    _res = 10 ** _fun(x, ymin, ymax)
-    return _res
-
-
-@jjit
-def _fun_fquench(x, x0, k, ymin, ymax):
-    fquench = _sigmoid(x, x0, k, ymin, ymax)
-    return fquench
-
-
-@jjit
-def _get_cov_scalar(chol_params):
-    chol = get_cholesky_from_params(jnp.array(chol_params))
-    cov = jnp.dot(chol, chol.T)
-    return cov
-
-
-@jjit
-def _get_mean_u_params_qseq(params, lgm):
-    ulgm = _fun_Mcrit(lgm, params.mean_ulgm_quench_ylo, params.mean_ulgm_quench_yhi)
-    ulgy = _fun(lgm, params.mean_ulgy_quench_ylo, params.mean_ulgy_quench_yhi)
-    ul = _fun(lgm, params.mean_ul_quench_ylo, params.mean_ul_quench_yhi)
-    utau = _fun(lgm, params.mean_utau_quench_ylo, params.mean_utau_quench_yhi)
-    uqt = _fun(lgm, params.mean_uqt_quench_ylo, params.mean_uqt_quench_yhi)
-    uqs = _fun(lgm, params.mean_uqs_quench_ylo, params.mean_uqs_quench_yhi)
-    udrop = _fun(lgm, params.mean_udrop_quench_ylo, params.mean_udrop_quench_yhi)
-    urej = _fun(lgm, params.mean_urej_quench_ylo, params.mean_urej_quench_yhi)
-    return ulgm, ulgy, ul, utau, uqt, uqs, udrop, urej
 
 
 @jjit
@@ -602,108 +453,8 @@ def _get_covariance_qseq_ms_block(params, lgm):
 
 
 @jjit
-def _get_chol_u_params_qseq_ms_block(params, lgm):
-    ulgm_ulgm = _fun_chol_diag(
-        lgm, params.chol_ulgm_ulgm_quench_ylo, params.chol_ulgm_ulgm_quench_yhi
-    )
-    ulgy_ulgy = _fun_chol_diag(
-        lgm, params.chol_ulgy_ulgy_quench_ylo, params.chol_ulgy_ulgy_quench_yhi
-    )
-    ul_ul = _fun_chol_diag(
-        lgm, params.chol_ul_ul_quench_ylo, params.chol_ul_ul_quench_yhi
-    )
-    utau_utau = _fun_chol_diag(
-        lgm, params.chol_utau_utau_quench_ylo, params.chol_utau_utau_quench_yhi
-    )
-
-    ulgy_ulgm = _fun(
-        lgm, params.chol_ulgy_ulgm_quench_ylo, params.chol_ulgy_ulgm_quench_yhi
-    )
-    ul_ulgm = _fun(lgm, params.chol_ul_ulgm_quench_ylo, params.chol_ul_ulgm_quench_yhi)
-    ul_ulgy = _fun(lgm, params.chol_ul_ulgy_quench_ylo, params.chol_ul_ulgy_quench_yhi)
-    utau_ulgm = _fun(
-        lgm, params.chol_utau_ulgm_quench_ylo, params.chol_utau_ulgm_quench_yhi
-    )
-    utau_ulgy = _fun(
-        lgm, params.chol_utau_ulgy_quench_ylo, params.chol_utau_ulgy_quench_yhi
-    )
-    utau_ul = _fun(lgm, params.chol_utau_ul_quench_ylo, params.chol_utau_ul_quench_yhi)
-
-    return (
-        ulgm_ulgm,
-        ulgy_ulgy,
-        ul_ul,
-        utau_utau,
-        ulgy_ulgm,
-        ul_ulgm,
-        ul_ulgy,
-        utau_ulgm,
-        utau_ulgy,
-        utau_ul,
-    )
-
-
-@jjit
-def _get_chol_u_params_qseq_q_block(params, lgm):
-    uqt_uqt = _fun_chol_diag(
-        lgm, params.chol_uqt_uqt_quench_ylo, params.chol_uqt_uqt_quench_yhi
-    )
-    uqs_uqs = _fun_chol_diag(
-        lgm, params.chol_uqs_uqs_quench_ylo, params.chol_uqs_uqs_quench_yhi
-    )
-    udrop_udrop = _fun_chol_diag(
-        lgm, params.chol_udrop_udrop_quench_ylo, params.chol_udrop_udrop_quench_yhi
-    )
-    urej_urej = _fun_chol_diag(
-        lgm, params.chol_urej_urej_quench_ylo, params.chol_urej_urej_quench_yhi
-    )
-    uqs_uqt = _fun(lgm, params.chol_uqs_uqt_quench_ylo, params.chol_uqs_uqt_quench_yhi)
-    udrop_uqt = _fun(
-        lgm, params.chol_udrop_uqt_quench_ylo, params.chol_udrop_uqt_quench_yhi
-    )
-    udrop_uqs = _fun(
-        lgm, params.chol_udrop_uqs_quench_ylo, params.chol_udrop_uqs_quench_yhi
-    )
-    urej_uqt = _fun(
-        lgm, params.chol_urej_uqt_quench_ylo, params.chol_urej_uqt_quench_yhi
-    )
-    urej_uqs = _fun(
-        lgm, params.chol_urej_uqs_quench_ylo, params.chol_urej_uqs_quench_yhi
-    )
-    urej_udrop = _fun(
-        lgm, params.chol_urej_udrop_quench_ylo, params.chol_urej_udrop_quench_yhi
-    )
-    return (
-        uqt_uqt,
-        uqs_uqs,
-        udrop_udrop,
-        urej_urej,
-        uqs_uqt,
-        udrop_uqt,
-        udrop_uqs,
-        urej_uqt,
-        urej_uqs,
-        urej_udrop,
-    )
-
-
-@jjit
-def _get_cov_qseq_ms_block(params, lgm):
-    chol_params_ms_block = _get_chol_u_params_qseq_ms_block(params, lgm)
-    cov_qseq_ms_block = _get_cov_scalar(chol_params_ms_block)
-    return cov_qseq_ms_block
-
-
-@jjit
-def _get_cov_qseq_q_block(params, lgm):
-    chol_params_q_block = _get_chol_u_params_qseq_q_block(params, lgm)
-    cov_qseq_q_block = _get_cov_scalar(chol_params_q_block)
-    return cov_qseq_q_block
-
-
-@jjit
 def _frac_quench_vs_lgm0(params, lgm0):
-    return _fun_fquench(
+    return _sigmoid(
         lgm0,
         params.frac_quench_x0,
         params.frac_quench_k,
@@ -750,36 +501,5 @@ def get_unbounded_qseq_params(params):
     )
     return QseqUParams(*u_params)
 
-
-@jjit
-def get_bounded_qseq_massonly_params(u_params):
-    fq_u_params = jnp.array([getattr(u_params, u_pname) for u_pname in _FRAC_Q_UPNAMES])
-    fq_params = _get_p_from_u_p_vmap(fq_u_params, jnp.array(FRAC_Q_BOUNDS))
-    params = QseqMassOnlyBlockParams(*u_params)
-    params = params._replace(frac_quench_x0=fq_params[0])
-    params = params._replace(frac_quench_k=fq_params[1])
-    params = params._replace(frac_quench_ylo=fq_params[2])
-    params = params._replace(frac_quench_yhi=fq_params[3])
-    return params
-
-
-@jjit
-def get_unbounded_qseq_massonly_params(params):
-    fq_params = jnp.array([getattr(params, pname) for pname in FRAC_Q_PNAMES])
-    fq_u_params = _get_u_p_from_p_vmap(fq_params, jnp.array(FRAC_Q_BOUNDS))
-
-    pnames = DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS._fields
-    params = jnp.array([getattr(params, pname) for pname in pnames])
-    u_params = QseqMassOnlyBlockUParams(*params)
-    u_params = u_params._replace(u_frac_quench_x0=fq_u_params[0])
-    u_params = u_params._replace(u_frac_quench_k=fq_u_params[1])
-    u_params = u_params._replace(u_frac_quench_ylo=fq_u_params[2])
-    u_params = u_params._replace(u_frac_quench_yhi=fq_u_params[3])
-    return u_params
-
-
-DEFAULT_SFH_PDF_QUENCH_BLOCK_U_PARAMS = QseqMassOnlyBlockUParams(
-    *get_unbounded_qseq_massonly_params(DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS)
-)
 
 SFH_PDF_QUENCH_U_PARAMS = QseqUParams(*get_unbounded_qseq_params(SFH_PDF_QUENCH_PARAMS))
--- diffstarpop/kernels/tests/test_qseq_massonly_block_cov.py
+++ diffstarpop/kernels/tests/test_qseq_massonly_block_cov.py
@@ -4,16 +4,10 @@
 import numpy as np
 import pytest
 from diffmah.utils import get_cholesky_from_params
-from jax import jit as jjit
 from jax import random as jran
-from jax import vmap
 
 from ...tests.test_utils import _enforce_is_cov
 from .. import qseq_massonly_block_cov as qseq
-
-get_cholesky_from_params_vmap = jjit(vmap(get_cholesky_from_params, in_axes=(0,)))
-_get_cov_qseq_ms_vmap = jjit(vmap(qseq._get_cov_qseq_ms_block, in_axes=(None, 0)))
-_get_cov_qseq_q_vmap = jjit(vmap(qseq._get_cov_qseq_q_block, in_axes=(None, 0)))
 
 EPSILON = 1e-5
 
@@ -70,154 +64,81 @@ def test_param_u_param_inversion():
     n_tests = 100
     for itest in range(n_tests):
         ran_key, test_key = jran.split(ran_key, 2)
-        n_p = len(qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS)
-        u_p = jran.uniform(test_key, minval=-10, maxval=10, shape=(n_p,))
-        u_p = qseq.QseqMassOnlyBlockUParams(*u_p)
-        p = qseq.get_bounded_qseq_massonly_params(u_p)
-        u_p2 = qseq.get_unbounded_qseq_massonly_params(p)
-        for x, y in zip(u_p, u_p2):
-            assert np.allclose(x, y, rtol=0.0001)
-
-
-@pytest.mark.xfail
-def test_covs_are_always_covs():
-    nhalos = 30
-    lgmarr = np.linspace(5, 20, nhalos)
-    ran_key = jran.key(0)
-    n_tests = 2_000
-    for itest in range(n_tests):
-        ran_key, test_key = jran.split(ran_key, 2)
-        n_p = len(qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS)
+        n_p = len(qseq.SFH_PDF_QUENCH_PARAMS)
         u_p = jran.uniform(test_key, minval=-100, maxval=100, shape=(n_p,))
-        u_p = qseq.QseqMassOnlyBlockUParams(*u_p)
-        p = qseq.get_bounded_qseq_massonly_params(u_p)
-        _res = qseq._get_qseq_means_and_covs_vmap(p, lgmarr)
-        for x in _res:
-            assert np.all(np.isfinite(x))
+        u_p = qseq.QseqUParams(*u_p)
+        p = qseq.get_bounded_qseq_params(u_p)
+        u_p2 = qseq.get_unbounded_qseq_params(p)
+        for x, y in zip(u_p, u_p2):
+            assert np.allclose(x, y, rtol=0.002)
 
-        mu_ms, cov_ms, mu_q, cov_q, frac_q = _res
-        assert mu_ms.shape == (nhalos, 4)
-        assert cov_ms.shape == (nhalos, 4, 4)
-        assert mu_q.shape == (nhalos, 4)
-        assert cov_q.shape == (nhalos, 4, 4)
-        assert frac_q.shape == (nhalos,)
 
-        assert np.all(frac_q >= -EPSILON)
-        assert np.all(frac_q <= 1 + EPSILON)
+def test_covs_are_always_covs_default_params():
+    for lgm in np.linspace(10, 15, 20):
+        cov_qseq_q_block = qseq._get_covariance_qseq_q_block(
+            qseq.SFH_PDF_QUENCH_PARAMS, lgm
+        )
+        assert cov_qseq_q_block.shape == (4, 4)
+        _enforce_is_cov(cov_qseq_q_block)
 
-        for matrix in cov_ms:
-            _enforce_is_cov(matrix)
-        for matrix in cov_q:
-            _enforce_is_cov(matrix)
+        cov_qseq_ms_block = qseq._get_covariance_qseq_ms_block(
+            qseq.SFH_PDF_QUENCH_PARAMS, lgm
+        )
+        assert cov_qseq_ms_block.shape == (4, 4)
+        _enforce_is_cov(cov_qseq_ms_block)
+
+
+def test_covs_are_always_covs_random_params():
+    lgmarr = np.linspace(10, 15, 20)
+    ran_key = jran.key(0)
+    npars = len(qseq.SFH_PDF_QUENCH_PARAMS)
+    ntests = 200
+    for __ in range(ntests):
+        ran_key, test_key = jran.split(ran_key, 2)
+        u_p = jran.uniform(test_key, minval=-1000, maxval=1000, shape=(npars,))
+        u_params = qseq.QseqUParams(*u_p)
+        params = qseq.get_bounded_qseq_params(u_params)
+
+        for lgm in lgmarr:
+            cov_qseq_ms_block = qseq._get_covariance_qseq_ms_block(params, lgm)
+            assert cov_qseq_ms_block.shape == (4, 4)
+            _enforce_is_cov(cov_qseq_ms_block)
+
+            cov_qseq_q_block = qseq._get_covariance_qseq_q_block(params, lgm)
+            assert cov_qseq_q_block.shape == (4, 4)
+            _enforce_is_cov(cov_qseq_q_block)
 
 
 def test_frac_quench_vs_lgm0():
     lgmarr = np.linspace(1, 20, 100)
-    fqarr = qseq._frac_quench_vs_lgm0(qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS, lgmarr)
+    fqarr = qseq._frac_quench_vs_lgm0(qseq.SFH_PDF_QUENCH_PARAMS, lgmarr)
     assert np.all(fqarr >= 0.0)
     assert np.all(fqarr <= 1.0)
 
 
-def test_default_frac_quench_params_are_in_bounds():
-    for key in qseq.FRAC_Q_PNAMES:
-        bounds = qseq.FRAC_Q_BOUNDS_PDICT[key]
-        val = qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PDICT[key]
+def test_default_params_are_in_bounds():
+    for key in qseq.SFH_PDF_QUENCH_PDICT.keys():
+        bounds = qseq.SFH_PDF_QUENCH_BOUNDS_PDICT[key]
+        val = qseq.SFH_PDF_QUENCH_PDICT[key]
         assert bounds[0] < val < bounds[1], key
 
 
 def test_params_u_params_inverts():
-    qseq_massonly_u_params = qseq.get_unbounded_qseq_massonly_params(
-        qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS
-    )
-    qseq_massonly_params = qseq.get_bounded_qseq_massonly_params(qseq_massonly_u_params)
-    assert np.allclose(qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS, qseq_massonly_params)
+    qseq_massonly_u_params = qseq.get_unbounded_qseq_params(qseq.SFH_PDF_QUENCH_PARAMS)
+    qseq_massonly_params = qseq.get_bounded_qseq_params(qseq_massonly_u_params)
+    assert np.allclose(qseq.SFH_PDF_QUENCH_PARAMS, qseq_massonly_params)
 
 
 def test_get_mean_u_params_qseq():
     lgmarr = np.linspace(11, 15, 100)
-    params = qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS
-    _means = qseq._get_mean_u_params_qseq(params, lgmarr)
-    ulgm, ulgy, ul, utau, uqt, uqs, udrop, urej = _means
+    params = qseq.SFH_PDF_QUENCH_PARAMS
+    _means = qseq._get_mean_u_params_qseq_ms_block(params, lgmarr)
+    assert len(_means) == 4
     for x in _means:
         assert np.all(np.isfinite(x))
-
-
-def test_get_chol_u_params_qseq_ms_block():
-    ngals = 100
-    lgmarr = np.linspace(11, 15, ngals)
-    params = qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS
-    _chols = qseq._get_chol_u_params_qseq_ms_block(params, lgmarr)
-    assert len(_chols) == 10
-    for x in _chols:
-        assert np.all(np.isfinite(x))
-        assert x.shape == (ngals,)
-
-    chol_params = np.array(_chols).T
-    assert chol_params.shape == (ngals, 10)
-
-    chols0 = get_cholesky_from_params(chol_params[0, :])
-    chols = get_cholesky_from_params_vmap(chol_params)
-    assert np.allclose(chols0, chols[0, :])
-    assert chols.shape == (ngals, 4, 4)
-
-
-def test_get_chol_u_params_qseq_q_block():
-    ngals = 100
-    lgmarr = np.linspace(11, 15, ngals)
-    params = qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS
-    _chols = qseq._get_chol_u_params_qseq_q_block(params, lgmarr)
-    assert len(_chols) == 10
-    for x in _chols:
-        assert np.all(np.isfinite(x))
-        assert x.shape == (ngals,)
-
-    chol_params = np.array(_chols).T
-    assert chol_params.shape == (ngals, 10)
-
-    chols0 = get_cholesky_from_params(chol_params[0, :])
-    chols = get_cholesky_from_params_vmap(chol_params)
-    assert np.allclose(chols0, chols[0, :])
-    assert chols.shape == (ngals, 4, 4)
-
-
-def test_get_cov_qseq_ms_block():
-    ngals = 100
-    lgmarr = np.linspace(11, 15, ngals)
-    params = qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS
-    cov_qseq0 = qseq._get_cov_qseq_ms_block(params, lgmarr[0])
-    covs_qseq = _get_cov_qseq_ms_vmap(params, lgmarr)
-    assert np.allclose(cov_qseq0, covs_qseq[0, :, :])
-    assert covs_qseq.shape == (ngals, 4, 4)
-    assert np.all(np.isfinite(covs_qseq))
-    for matrix in covs_qseq:
-        _enforce_is_cov(matrix)
-
-
-def test_sub_block_param_dicts_have_expected_dimension():
-    ms_block_dict = qseq.DEFAULT_SFH_PDF_QUENCH_MS_BLOCK_PDICT
-    q_block_dict = qseq.DEFAULT_SFH_PDF_QUENCH_Q_BLOCK_PDICT
-    n_entries_chol_44 = 4 * (4 + 1) / 2
-    n_params_chol_44 = 2 * n_entries_chol_44
-    n_params_mean_44 = 2 * 4
-    n_params_block_44 = n_params_chol_44 + n_params_mean_44
-    assert len(ms_block_dict) == len(q_block_dict) == n_params_block_44
-    assert set(ms_block_dict.keys()) & set(q_block_dict.keys()) == set()
-
-
-def test_get_chol_u_params_qseq_block():
-    n_gals = 50
-    lgmarr = np.linspace(10, 15, n_gals)
-    params = qseq.DEFAULT_SFH_PDF_QUENCH_BLOCK_PARAMS
-    _chol_pars_q_block = qseq._get_chol_u_params_qseq_q_block(params, lgmarr)
-    assert len(_chol_pars_q_block) == 4 * (4 + 1) / 2
-    for x in _chol_pars_q_block:
-        assert x.shape == (n_gals,)
-        assert np.all(np.isfinite(x))
-
-    _chol_pars_ms_block = qseq._get_chol_u_params_qseq_ms_block(params, lgmarr)
-    assert len(_chol_pars_ms_block) == 4 * (4 + 1) / 2
-    for x in _chol_pars_ms_block:
-        assert x.shape == (n_gals,)
+    _means = qseq._get_mean_u_params_qseq_q_block(params, lgmarr)
+    assert len(_means) == 4
+    for x in _means:
         assert np.all(np.isfinite(x))
 
 
@@ -277,43 +198,3 @@ def test_get_cov_params_qseq_q_block():
         assert np.all(np.isfinite(x))
     for x in off_diags:
         assert np.all(np.isfinite(x))
-
-
-def test_get_covariance_qseq_q_block_default_params():
-    for lgm in np.linspace(10, 15, 20):
-        cov_qseq_q_block = qseq._get_covariance_qseq_q_block(
-            qseq.SFH_PDF_QUENCH_PARAMS, lgm
-        )
-        assert cov_qseq_q_block.shape == (4, 4)
-        _enforce_is_cov(cov_qseq_q_block)
-
-
-def test_get_covariance_qseq_ms_block_default_params():
-    lgmarr = np.linspace(10, 15, 20)
-    for lgm in lgmarr:
-        cov_qseq_ms_block = qseq._get_covariance_qseq_ms_block(
-            qseq.SFH_PDF_QUENCH_PARAMS, lgm
-        )
-        assert cov_qseq_ms_block.shape == (4, 4)
-        _enforce_is_cov(cov_qseq_ms_block)
-
-
-def test_get_covariance_qseq_random_params():
-    lgmarr = np.linspace(10, 15, 20)
-    ran_key = jran.key(0)
-    npars = len(qseq.SFH_PDF_QUENCH_PARAMS)
-    ntests = 200
-    for __ in range(ntests):
-        ran_key, test_key = jran.split(ran_key, 2)
-        u_p = jran.uniform(test_key, minval=-1000, maxval=1000, shape=(npars,))
-        u_params = qseq.QseqUParams(*u_p)
-        params = qseq.get_bounded_qseq_params(u_params)
-
-        for lgm in lgmarr:
-            cov_qseq_ms_block = qseq._get_covariance_qseq_ms_block(params, lgm)
-            assert cov_qseq_ms_block.shape == (4, 4)
-            _enforce_is_cov(cov_qseq_ms_block)
-
-            cov_qseq_q_block = qseq._get_covariance_qseq_q_block(params, lgm)
-            assert cov_qseq_q_block.shape == (4, 4)
-            _enforce_is_cov(cov_qseq_q_block)
