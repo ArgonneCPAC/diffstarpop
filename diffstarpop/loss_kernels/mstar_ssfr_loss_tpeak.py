87b6bc688d1a556dabd7ed8432e9ffad3231377d
Alex Alarcon <alexalarcongonzalez@gmail.com>
2024-10-08 01:12:49 +0200

    Adding first kernels for fitting mstar PDFs

---
+++ diffstarpop/loss_kernels/mstar_ssfr_loss_tpeak.py
@@ -0,0 +1,188 @@
+"""
+"""
+
+from collections import OrderedDict, namedtuple
+from diffmah.diffmah_kernels import mah_halopop
+from diffstar.utils import cumulative_mstar_formed
+import jax
+from jax import jit as jjit
+from jax import numpy as jnp
+from jax import vmap
+from jax import value_and_grad
+
+from ..mc_diffstarpop_tpeak_block_cov import mc_diffstar_sfh_galpop
+from ..sumstats.smhm import compute_smhm
+from ..kernels.defaults_tpeak_block_cov import (
+    get_bounded_diffstarpop_params,
+    DEFAULT_DIFFSTARPOP_U_PARAMS
+)
+from .namedtuple_utils import (
+    tuple_to_jax_array,
+    array_to_tuple_new_diffstarpop,
+)
+from diffmah import DiffmahParams
+from diffsky.diffndhist import tw_ndhist_weighted
+
+
+N_TIMES = 20
+
+_A = (None, 0)
+cumulative_mstar_formed_halopop = jjit(vmap(cumulative_mstar_formed, in_axes=_A))
+
+unbound_params_dict = OrderedDict(
+    diffstarpop_u_params=DEFAULT_DIFFSTARPOP_U_PARAMS
+)
+UnboundParams = namedtuple(
+    "UnboundParams", list(unbound_params_dict.keys()))
+
+@jjit
+def _mse(pred, target):
+    diff = pred - target
+    return jnp.mean(diff**2)
+
+def _calculate_obs_data_kern(
+    tobs_target,
+    sfh_ms,
+    sfh_q,
+):
+    tarr = jnp.logspace(-1, jnp.log10(tobs_target), N_TIMES)
+    smh_ms = cumulative_mstar_formed_halopop(tarr, sfh_ms)
+    smh_q = cumulative_mstar_formed_halopop(tarr, sfh_q)
+    smh_ms_tobs = smh_ms[:, -1]
+    smh_q_tobs = smh_q[:, -1]
+    return smh_ms_tobs, smh_q_tobs
+    
+calculate_obs_data = jjit(vmap(_calculate_obs_data_kern, in_axes=(0,0,0)))
+
+def _mc_diffstar_sfh_galpop_vmap_kern(
+    diffstarpop_params,
+    mah_params,
+    t_peak,
+    lgmu_infall,
+    logmhost_infall,
+    gyr_since_infall,
+    ran_key,
+    tobs_target,
+):
+    mah_params = DiffmahParams(*mah_params)
+    tarr = jnp.logspace(-1, jnp.log10(tobs_target), N_TIMES)
+    res = mc_diffstar_sfh_galpop(
+        diffstarpop_params,
+        mah_params,
+        t_peak,
+        lgmu_infall,
+        logmhost_infall,
+        gyr_since_infall,
+        ran_key,
+        tarr
+    )
+    return res
+
+
+_U = (None, *[0]*7)
+mc_diffstar_sfh_galpop_vmap = jjit(vmap(_mc_diffstar_sfh_galpop_vmap_kern, in_axes=_U))
+
+@jjit
+def compute_diff_histograms_mstar_atmobs_z(
+    logmstar_bins,
+    log_smh_table,
+    weight,
+):
+
+    n_halos = log_smh_table.shape[0]
+
+    nddata = log_smh_table.reshape((-1, 1))
+
+    sigma = jnp.mean(jnp.diff(logmstar_bins)) + jnp.zeros(n_halos)
+    ndsig = sigma.reshape((-1, 1))
+
+    ydata = log_smh_table.reshape((-1, 1))
+
+    ndbins_lo = logmstar_bins[:-1].reshape((-1, 1))
+    ndbins_hi = logmstar_bins[1:].reshape((-1, 1))
+
+    wcounts = tw_ndhist_weighted(nddata, ndsig, weight, ndbins_lo, ndbins_hi)
+
+    wcounts = wcounts / jnp.sum(wcounts)
+
+    return wcounts
+
+compute_diff_histograms_mstar_atmobs_z_vmap = jjit(vmap(compute_diff_histograms_mstar_atmobs_z, in_axes=(None, 0, 0)))
+
+@jjit
+def mstar_ssfr_kern_tobs(u_params, loss_data):
+    (
+        mah_params,
+        t_peak,
+        lgmu_infall,
+        logmhost_infall,
+        gyr_since_infall,
+        ran_key,
+        tobs_target,
+        logmstar_bins,
+        target_mstar_pdf,
+    ) = loss_data
+
+    diffstarpop_params = get_bounded_diffstarpop_params(u_params.diffstarpop_u_params)
+    
+    _res = mc_diffstar_sfh_galpop_vmap(
+        diffstarpop_params,
+        mah_params,
+        t_peak,
+        lgmu_infall,
+        logmhost_infall,
+        gyr_since_infall,
+        ran_key,
+        tobs_target,
+    )
+    diffstar_params_ms, diffstar_params_q, sfh_ms, sfh_q, frac_q, mc_is_q = _res
+
+    smh_ms_tobs, smh_q_tobs = calculate_obs_data(tobs_target, sfh_ms, sfh_q)
+    sfh_ms_tobs, sfh_q_tobs = sfh_ms[:,:,-1], sfh_q[:,:,-1]
+
+    log_sfh_ms = jnp.log10(sfh_ms_tobs)
+    log_sfh_q = jnp.log10(sfh_q_tobs)
+    log_smh_ms = jnp.log10(smh_ms_tobs)
+    log_smh_q = jnp.log10(smh_q_tobs)
+
+    log_ssfrh_ms = log_sfh_ms - log_smh_ms
+    log_ssfrh_q = log_sfh_q - log_smh_q
+
+    weight_q = jnp.ones_like(log_sfh_q) * frac_q
+    weight_ms = jnp.ones_like(log_sfh_ms) * (1 - frac_q)
+
+    log_smh = jnp.concatenate((log_smh_ms, log_smh_q), axis=1)
+    weight_smh = jnp.concatenate((weight_ms, weight_q), axis=1)
+
+    pred_mstar_pdf = compute_diff_histograms_mstar_atmobs_z_vmap(
+        logmstar_bins,
+        log_smh,
+        weight_smh,
+    )
+
+    return pred_mstar_pdf 
+
+@jjit
+def loss_mstar_ssfr_kern_tobs(u_params, loss_data):
+    target_mstar_pdf = loss_data[-1]
+    pred_mstar_pdf  = mstar_ssfr_kern_tobs(u_params, loss_data)
+
+    return _mse(pred_mstar_pdf, target_mstar_pdf)
+
+loss_mstar_ssfr_kern_tobs_grad_kern = jjit(value_and_grad(loss_mstar_ssfr_kern_tobs, argnums=(0,)))
+
+
+def loss_mstar_ssfr_kern_tobs_grad_wrapper(flat_uparams, loss_data):
+        
+    namedtuple_uparams = array_to_tuple_new_diffstarpop(flat_uparams, UnboundParams)
+    loss, grads = loss_mstar_ssfr_kern_tobs_grad_kern(namedtuple_uparams, loss_data)
+    grads = tuple_to_jax_array(grads)
+
+    return loss, grads
+
+def get_pred_data_wrapper(flat_uparams, loss_data):
+        
+    namedtuple_uparams = array_to_tuple_new_diffstarpop(flat_uparams, UnboundParams)
+    pred_mstar_pdf  = mstar_ssfr_kern_tobs(namedtuple_uparams, loss_data)
+
+    return pred_mstar_pdf
---
+++ scripts/fit_mstar_pdfs.py
@@ -0,0 +1,253 @@
+import h5py
+import numpy as np
+from jax import (
+    numpy as jnp,
+    jit as jjit,
+    random as jran,
+    grad,
+    vmap,
+)
+import argparse
+from time import time
+from scipy.optimize import minimize
+from jax.example_libraries import optimizers as jax_opt
+
+from collections import OrderedDict, namedtuple
+
+from diffstarpop.loss_kernels.mstar_ssfr_loss_tpeak import (
+    loss_mstar_ssfr_kern_tobs_grad_wrapper,
+    UnboundParams,
+)
+
+from diffstarpop.loss_kernels.namedtuple_utils import (
+    tuple_to_array,
+    register_tuple_new_diffstarpop,
+    array_to_tuple_new_diffstarpop,
+)
+from diffstarpop.kernels.defaults_tpeak_block_cov import (
+    DEFAULT_DIFFSTARPOP_U_PARAMS,
+    DEFAULT_DIFFSTARPOP_PARAMS,
+    get_bounded_diffstarpop_params,
+)
+
+BEBOP_SMHM_MEAN_DATA = "/lcrc/project/halotools/alarcon/results/"
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-indir", help="input drn", type=str, default=BEBOP_SMHM_MEAN_DATA
+    )
+    parser.add_argument(
+        "-outdir", help="output drn", type=str, default=BEBOP_SMHM_MEAN_DATA
+    )
+    parser.add_argument(
+        "-make_plot", help="whether to make plot", type=bool, default=False
+    )
+    parser.add_argument(
+        "-nhalos", help="Number of halos for fitting", type=int, default=100
+    )
+
+    args = parser.parse_args()
+    indir = args.indir
+    outdir = args.outdir
+    make_plot = args.make_plot
+    nhalos = args.nhalos
+
+    # Load SMHM data ---------------------------------------------
+    print("Loading SMHM data...")
+
+
+    with h5py.File(indir+"smdpl_smhm.h5", "r") as hdf:
+        redshift_targets = hdf["redshift_targets"][:]
+        smhm_diff = hdf["smhm_diff"][:]
+        smhm = hdf["smhm"][:]
+        logmh_bins = hdf["logmh_bins"][:]
+        age_targets = hdf["age_targets"][:]
+        """ 
+            hdfout["counts_diff"] = wcounts
+            hdfout["hist_diff"] = whist
+            hdfout["counts"] = counts
+            hdfout["hist"] = hist
+            hdfout["smhm_diff"] = whist / wcounts
+            hdfout["smhm"] = hist / counts
+            hdfout["logmh_bins"] = smhm_utils.LOGMH_BINS
+            hdfout["subvol_used"] = subvol_used
+            
+        """
+
+    logmh_binsc = 0.5*(logmh_bins[1:]+logmh_bins[:-1])
+
+
+    with h5py.File(indir+"smdpl_smhm_samples_haloes.h5", "r") as hdf:
+        logmh_id = hdf["logmh_id"][:]
+        logmh_val = hdf["logmh_id"][:]
+        mah_params_samp = hdf["mah_params_samp"][:]
+        ms_params_samp = hdf["ms_params_samp"][:]
+        q_params_samp = hdf["q_params_samp"][:]
+        t_peak_samp = hdf["t_peak_samp"][:]
+        tobs_id = hdf["tobs_id"][:]
+        tobs_val = hdf["tobs_val"][:]
+        redshift_val = hdf["redshift_val"][:]
+
+
+    tpeak_path = "/Users/alarcon/Documents/diffmah_data/tpeak/random_data_241007/"
+    with h5py.File(tpeak_path+"smdpl_mstar_ssfr.h5", "r") as hdf:
+        mstar_wcounts = hdf["mstar_wcounts"][:]
+        mstar_counts = hdf["mstar_counts"][:]
+        mstar_ssfr_wcounts_cent = hdf["mstar_ssfr_wcounts_cent"][:]
+        mstar_ssfr_wcounts_sat = hdf["mstar_ssfr_wcounts_sat"][:]
+        logssfr_bins_pdf = hdf["logssfr_bins_pdf"][:]
+        logmstar_bins_pdf = hdf["logmstar_bins_pdf"][:]
+        """
+        hdfout["mstar_wcounts"] = mstar_wcounts
+        hdfout["mstar_counts"] = mstar_counts
+        hdfout["mstar_ssfr_wcounts_cent"] = mstar_ssfr_wcounts_cent
+        hdfout["mstar_ssfr_wcounts_sat"] = mstar_ssfr_wcounts_sat
+        hdfout["logmh_bins"] = smhm_utils.LOGMH_BINS
+        hdfout["logmstar_bins_pdf"] = smhm_utils.LOGMSTAR_BINS_PDF
+        hdfout["logssfr_bins_pdf"] = smhm_utils.LOGSSFR_BINS_PDF
+        hdfout["redshift_targets"] = redshift_targets
+        hdfout["age_targets"] = age_targets
+        """
+
+    logssfr_binsc_pdf = 0.5*(logssfr_bins_pdf[1:]+logssfr_bins_pdf[:-1])
+    logmstar_binsc_pdf = 0.5*(logmstar_bins_pdf[1:]+logmstar_bins_pdf[:-1])
+
+
+    # Create loss_data ---------------------------------------------
+    print("Creating loss data...")
+
+    ran_key = jran.PRNGKey(np.random.randint(2**32))
+
+    lgmu_infall = -1.0
+    logmhost_infall = 13.0
+    gyr_since_infall = 2.0
+
+    mah_params_data = []
+    t_peak_data = []
+    lgmu_infall_data = []
+    logmhost_infall_data = []
+    gyr_since_infall_data = []
+    t_obs_targets = []
+    mstar_counts_target = []
+
+    for i in range(len(age_targets)):
+        t_target = age_targets[i]
+
+        for j in range(len(logmh_binsc)):
+            sel = (tobs_id == i)  & (logmh_id == j)
+
+            if sel.sum() < nhalos: continue
+            arange_sel = np.arange(len(tobs_id))[sel]
+            arange_sel = np.random.choice(arange_sel, nhalos, replace=False)
+            mah_params_data.append(mah_params_samp[:, arange_sel])
+            t_peak_data.append(t_peak_samp[arange_sel])
+            lgmu_infall_data.append(np.ones(len(arange_sel))*lgmu_infall)
+            logmhost_infall_data.append(np.ones(len(arange_sel))*logmhost_infall)
+            gyr_since_infall_data.append(np.ones(len(arange_sel))*gyr_since_infall)
+            t_obs_targets.append(t_target)
+            mstar_counts_target.append(mstar_wcounts[i,j]/mstar_wcounts[i,j].sum())
+        break
+
+    mah_params_data = np.array(mah_params_data)
+    t_peak_data = np.array(t_peak_data)
+    lgmu_infall_data = np.array(lgmu_infall_data)
+    logmhost_infall_data = np.array(logmhost_infall_data)
+    gyr_since_infall_data = np.array(gyr_since_infall_data)
+    t_obs_targets = np.array(t_obs_targets)
+    mstar_counts_target = np.array(mstar_counts_target)
+
+    ran_key_data = jran.split(ran_key, len(mstar_counts_target))
+    loss_data = (
+        mah_params_data,
+        t_peak_data,
+        lgmu_infall_data,
+        logmhost_infall_data,
+        gyr_since_infall_data,
+        ran_key_data,
+        t_obs_targets,
+        logmstar_bins_pdf,
+        mstar_counts_target
+    )
+
+    # Register params ---------------------------------------------
+
+
+    unbound_params_dict = OrderedDict(
+        diffstarpop_u_params=DEFAULT_DIFFSTARPOP_U_PARAMS
+    )
+    UnboundParams = namedtuple(
+        "UnboundParams", list(unbound_params_dict.keys()))
+    register_tuple_new_diffstarpop(UnboundParams)
+    all_u_params = UnboundParams(*list(unbound_params_dict.values()))
+
+    # Run fitter ---------------------------------------------
+    print("Running fitter...")
+
+    params_init = tuple_to_array(all_u_params)
+    loss_mstar_ssfr_kern_tobs_grad_wrapper(params_init, loss_data)
+
+    start = time()
+
+    n_step = int(1e4)
+    step_size=0.01
+
+    loss_arr = np.zeros(n_step).astype("f4") + np.inf
+
+    opt_init, opt_update, get_params = jax_opt.adam(step_size)
+    opt_state = opt_init(params_init)
+
+    n_params = len(params_init)
+    params_arr = np.zeros((n_step, n_params)).astype("f4")
+
+    n_mah = 100
+
+    no_nan_grads_arr = np.zeros(n_step)
+    for istep in range(n_step):
+        start = time()
+        ran_key, subkey = jran.split(ran_key, 2)
+
+        p = np.array(get_params(opt_state))
+
+        loss, grads = loss_mstar_ssfr_kern_tobs_grad_wrapper(
+            p, loss_data
+        )
+        
+        no_nan_params = np.all(np.isfinite(p))
+        no_nan_loss = np.isfinite(loss)
+        no_nan_grads = np.all(np.isfinite(grads))
+        if ~no_nan_params | ~no_nan_loss | ~no_nan_grads:
+            # break
+            if istep > 0:
+                indx_best = np.nanargmin(loss_arr[:istep])
+                best_fit_params = params_arr[indx_best]
+                best_fit_loss = loss_arr[indx_best]
+            else:
+                best_fit_params = np.copy(p)
+                best_fit_loss = 999.99
+        else:
+            params_arr[istep, :] = p
+            loss_arr[istep] = loss
+            opt_state = opt_update(istep, grads, opt_state)
+            
+        
+        no_nan_grads_arr[istep] = ~no_nan_grads
+        end = time()
+        if istep%100==0:
+            print(istep, loss, end-start, no_nan_grads)
+        if ~no_nan_grads:
+            break
+
+    argmin_best = np.argmin(loss_arr)
+    best_fit_params = params_arr[argmin_best]
+
+
+    def return_params_from_result(bestfit):
+            bestfit_u_tuple = array_to_tuple_new_diffstarpop(bestfit, UnboundParams)
+            diffstarpop_params = get_bounded_diffstarpop_params(bestfit_u_tuple.diffstarpop_u_params)
+            return diffstarpop_params
+    
+    best_result = return_params_from_result(best_fit_params)
+    best_result = tuple_to_array(best_result)
+    np.save(outdir+"bestfit_diffstarpop_params.npy", best_result)
