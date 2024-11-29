5f588466678b7a78491d1bc982934905cce28c65
Alex Alarcon <alexalarcongonzalez@gmail.com>
2024-11-29 16:56:54 +0100

    Adding a couple more tests

--- diffstarpop/loss_kernels/tests/test_smhm_loss_tpeak.py
+++ diffstarpop/loss_kernels/tests/test_smhm_loss_tpeak.py
@@ -6,15 +6,16 @@ from diffmah.diffmah_kernels import DEFAULT_MAH_PARAMS
 from jax import random as jran
 from collections import OrderedDict, namedtuple
 
-from ...kernels.defaults_tpeak import DEFAULT_DIFFSTARPOP_U_PARAMS
+from ...kernels.defaults_tpeak import DEFAULT_DIFFSTARPOP_U_PARAMS, DEFAULT_DIFFSTARPOP_PARAMS
 from .. import smhm_loss_tpeak as smhm_loss
 from ..namedtuple_utils_tpeak import (
     tuple_to_array,
     register_tuple_new_diffstarpop_tpeak,
 )
+from ...mc_diffstarpop_tpeak import mc_diffstar_sfh_galpop
 
 
-def test_smhm_loss_and_grads_are_finite():
+def get_test_loss_data():
     n_halos = 100
     n_targets = 50
     ZZ = np.zeros((n_targets, n_halos))
@@ -45,6 +46,12 @@ def test_smhm_loss_and_grads_are_finite():
         t_obs_targets,
         smhm_targets,
     )
+    return loss_data
+
+
+def test_smhm_loss_and_grads_are_finite():
+    
+    loss_data = get_test_loss_data()
 
     # Register params ---------------------------------------------
 
@@ -61,3 +68,78 @@ def test_smhm_loss_and_grads_are_finite():
     assert loss > 0, "Loss is not positive!"
     assert np.isfinite(grads).all(), "Some gradients are not finite!"
     assert np.any(~(grads == np.zeros_like(grads))), "Some gradients are zero!"
+
+
+def test_mean_smhm_loss_kern():
+
+    n_halos = 100
+    ZZ = np.zeros(n_halos)
+
+    ran_key = jran.PRNGKey(np.random.randint(2**32))
+    lgmu_infall = -1.0 + ZZ
+    logmhost_infall = 13.0 + ZZ
+    gyr_since_infall  = 2.0 + ZZ
+
+    t_table = np.linspace(1.0, 13.8, 100)
+
+    mah_params = DEFAULT_MAH_PARAMS._make([ZZ + x for x in DEFAULT_MAH_PARAMS])
+    logm0 = np.random.uniform(low=11.0, high=15.0, size=(n_halos))
+    mean_logsm_target = np.random.uniform(low=9.0, high=1.0, size=(n_halos))
+    mah_params = mah_params._replace(logm0=logm0)
+    mah_params = np.array(mah_params)
+
+    loss_data = (
+        mah_params,
+        logm0,
+        lgmu_infall,
+        logmhost_infall,
+        gyr_since_infall,
+        ran_key,
+        t_table,
+        mean_logsm_target,           
+    )
+
+    loss = smhm_loss.mean_smhm_loss_kern(DEFAULT_DIFFSTARPOP_PARAMS, loss_data)
+    assert loss > 0, "Loss is not positive!"
+
+def test_cumulative_mstar_formed_halopop():
+
+    n_halos = 100
+    ZZ = np.zeros(n_halos)
+
+    ran_key = jran.PRNGKey(np.random.randint(2**32))
+    lgmu_infall = -1.0 + ZZ
+    logmhost_infall = 13.0 + ZZ
+    gyr_since_infall  = 2.0 + ZZ
+
+    t_table = np.linspace(1.0, 13.8, 100)
+
+    mah_params = DEFAULT_MAH_PARAMS._make([ZZ + x for x in DEFAULT_MAH_PARAMS])
+    logm0 = np.random.uniform(low=11.0, high=15.0, size=(n_halos))
+    mean_logsm_target = np.random.uniform(low=9.0, high=1.0, size=(n_halos))
+    mah_params = mah_params._replace(logm0=logm0)
+    mah_params = np.array(mah_params)
+
+    _res = mc_diffstar_sfh_galpop(
+        DEFAULT_DIFFSTARPOP_PARAMS,
+        mah_params,
+        logm0,
+        lgmu_infall,
+        logmhost_infall,
+        gyr_since_infall,
+        ran_key,
+        t_table,
+    )
+    diffstar_params_ms, diffstar_params_q, sfh_q, sfh_ms, frac_q, mc_is_q = _res
+
+    smh_ms = smhm_loss.cumulative_mstar_formed_halopop(t_table, sfh_ms)
+    smh_q = smhm_loss.cumulative_mstar_formed_halopop(t_table, sfh_q)
+
+    assert np.isfinite(smh_ms).all()
+    assert np.isfinite(smh_q).all()
+
+    assert (smh_ms >= 0.0).all()
+    assert (smh_q >= 0.0).all()
+
+
+
--- diffstarpop/tests/test_mc_diffstarpop_tpeak.py
+++ diffstarpop/tests/test_mc_diffstarpop_tpeak.py
@@ -115,25 +115,39 @@ def test_mc_diffstar_u_params_galpop():
 #     diffstar_params_ms, diffstar_params_q, frac_q, mc_is_q = _res
 
 
-# def test_mc_diffstar_sfh_galpop():
-#     ngals = 50
-#     zz = np.zeros(ngals)
-#     lgmu_infall = -1.0 + zz
-#     logmhost_infall = 13.0 + zz
-#     gyr_since_infall = 2.0 + zz
-#     ran_key = jran.key(0)
-#     mah_params = DEFAULT_MAH_PARAMS._make([zz + p for p in DEFAULT_MAH_PARAMS])
-#     t_peak = zz + 12.1
-#     n_times = 100
-#     tarr = np.linspace(0.1, 13.8, n_times)
-#     _res = mcdsp.mc_diffstar_sfh_galpop(
-#         DEFAULT_DIFFSTARPOP_PARAMS,
-#         mah_params,
-#         t_peak,
-#         lgmu_infall,
-#         logmhost_infall,
-#         gyr_since_infall,
-#         ran_key,
-#         tarr,
-#     )
-#     diffstar_params_ms, diffstar_params_q, sfh_q, sfh_ms, frac_q, mc_is_q = _res
+def test_mc_diffstar_sfh_galpop():
+    n_halos = 100
+    ZZ = np.zeros(n_halos)
+
+    ran_key = jran.PRNGKey(np.random.randint(2**32))
+    lgmu_infall = -1.0 + ZZ
+    logmhost_infall = 13.0 + ZZ
+    gyr_since_infall  = 2.0 + ZZ
+
+    t_table = np.linspace(1.0, 13.8, 100)
+
+    mah_params = DEFAULT_MAH_PARAMS._make([ZZ + x for x in DEFAULT_MAH_PARAMS])
+    logm0 = np.random.uniform(low=11.0, high=15.0, size=(n_halos))
+    mean_logsm_target = np.random.uniform(low=9.0, high=1.0, size=(n_halos))
+    mah_params = mah_params._replace(logm0=logm0)
+    mah_params = np.array(mah_params)
+
+    _res = mcdsp.mc_diffstar_sfh_galpop(
+        DEFAULT_DIFFSTARPOP_PARAMS,
+        mah_params,
+        logm0,
+        lgmu_infall,
+        logmhost_infall,
+        gyr_since_infall,
+        ran_key,
+        t_table,
+    )
+    diffstar_params_ms, diffstar_params_q, sfh_q, sfh_ms, frac_q, mc_is_q = _res
+
+    assert np.isfinite(sfh_q).all()
+    assert np.isfinite(sfh_ms).all()
+    assert np.isfinite(frac_q).all()
+
+    assert (sfh_q >= 0.0).all()
+    assert (sfh_ms >= 0.0).all()
+    assert (frac_q >= 0.0).all()
