6cb2ef7519dff3cbbc8fcc708348628cc06d51f5
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-03-05 12:11:21 +0100

    Adding smoothly_clipped_line model for Diffstarpop.

--- diffstarpop/utils.py
+++ diffstarpop/utils.py
@@ -176,3 +176,53 @@ def covariance_from_correlation(corr, evals):
     D = jnp.diag(evals)
     cov = jnp.dot(jnp.dot(D, corr), D)
     return cov
+
+
+@jjit
+def smoothly_clipped_line(x, x0, y0, m, y_lo, y_hi):
+    """
+    Generates a smoothly clipped straight line with specified parameters.
+
+    This function computes a line with an initial intercept `y0`, a slope `m`,
+    and a zero point at `x0`. The line is smoothly clipped within the bounds
+    `y_lo` and `y_hi` using a sigmoid-based transition to avoid sharp cutoffs.
+
+    Parameters:
+    x : array-like
+        The x-axis values at which to evaluate the function.
+    x0 : float
+        The reference x-value where y = y0.
+    y0 : float
+        The initial y-intercept of the line at x = x0.
+    m : float
+        The slope of the line.
+    y_lo : float
+        The lower bound for the y-values.
+    y_hi : float
+        The upper bound for the y-values.
+
+    Returns:
+    y_clipped : array-like
+        The y-values corresponding to x, smoothly clipped within [y_lo, y_hi].
+
+    """
+    x_lo = (y_lo - y0) / m + x0  # value of x at which y=y_lo
+    x_hi = (y_hi - y0) / m + x0  # value of x at which y=y_hi
+
+    dx_lo = x0 - x_lo
+    dx_hi = x_hi - x0
+
+    eps_lo = dx_lo / 100.0  # tiny step in x above x_lo
+    eps_hi = dx_hi / 100.0  # tiny step in x below x_hi
+
+    xc_lo = x_lo + eps_lo  # value of x at which y=y_lo+ε
+    xc_hi = x_hi - eps_hi  # value of x at which y=y_hi-ε
+
+    y_lo_bound = y0 + m * (xc_lo - x0)  # value of y=y_lo+ε
+    y_hi_bound = y0 + m * (xc_hi - x0)  # value of y=y_hi-ε
+
+    CLIPPING_K = 20.0 / eps_lo  # Steep transition speed for this Δx
+    y_unclipped = y0 + m * (x - x0)
+    y_clipped_from_below = _sigmoid(x, xc_lo, CLIPPING_K, y_lo_bound, y_unclipped)
+    y_clipped = _sigmoid(x, xc_hi, CLIPPING_K, y_clipped_from_below, y_hi_bound)
+    return y_clipped
