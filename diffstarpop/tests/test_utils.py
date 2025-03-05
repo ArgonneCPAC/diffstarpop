483c25456235d19a20fd93061476ee0df5fd141f
Andrew Hearin <ahearin@anl.gov>
2025-03-05 07:53:34 -0600

    Change import in test_utils to simplify adding future tests

--- diffstarpop/tests/test_utils.py
+++ diffstarpop/tests/test_utils.py
@@ -5,11 +5,7 @@ import numpy as np
 from jax import random as jran
 from scipy.stats import random_correlation
 
-from ..utils import (
-    correlation_from_covariance,
-    covariance_from_correlation,
-    get_t50_p50,
-)
+from .. import utils as ut
 
 
 def _enforce_is_cov(matrix):
@@ -33,7 +29,7 @@ def test_get_t50_p50_evaluates():
     threshold = 0.5
     logmpeak = histories[:, -1]
     try:
-        t50, p50 = get_t50_p50(t_table, 10**histories, threshold, logmpeak)
+        t50, p50 = ut.get_t50_p50(t_table, 10**histories, threshold, logmpeak)
         assert t50.shape == (ngals,)
         assert p50.shape == (ngals,)
     except ImportError:
@@ -47,9 +43,9 @@ def test_correlation_from_covariance():
         evals = np.sort(np.random.uniform(0, 100, ndim))
         evals = ndim * evals / evals.sum()
         corr_matrix = random_correlation.rvs(evals)
-        cov_matrix = covariance_from_correlation(corr_matrix, evals)
+        cov_matrix = ut.covariance_from_correlation(corr_matrix, evals)
         S = np.sqrt(np.diag(cov_matrix))
         assert np.allclose(S, evals, rtol=1e-4)
-        inferred_corr_matrix = correlation_from_covariance(cov_matrix)
+        inferred_corr_matrix = ut.correlation_from_covariance(cov_matrix)
         assert np.allclose(corr_matrix, inferred_corr_matrix, rtol=1e-4)
         _enforce_is_cov(cov_matrix)
