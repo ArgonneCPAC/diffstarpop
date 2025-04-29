16a4a27855e8acb3c99e323f1186f24b528b029c
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-04-29 17:28:05 +0200

    Changing defaults to use defaults_tpeak_line_sepms_satfrac

--- diffstarpop/defaults.py
+++ diffstarpop/defaults.py
@@ -1,9 +1,8 @@
-"""
-"""
+""" """
 
 # flake8: noqa
 
-from .kernels.defaults_tpeak_line import (
+from .kernels.defaults_tpeak_line_sepms_satfrac import (
     DEFAULT_DIFFSTARPOP_PARAMS,
     DEFAULT_DIFFSTARPOP_U_PARAMS,
     DiffstarPopParams,
