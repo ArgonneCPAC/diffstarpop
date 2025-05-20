db3961cad825588e49e5615c4287473be2d12cd0
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-05-20 11:14:11 +0200

    Extending stellar mass up to 1e13

--- scripts/tng_smhm_utils.py
+++ scripts/tng_smhm_utils.py
@@ -1,5 +1,4 @@
-"""
-"""
+""" """
 
 import os
 
@@ -21,7 +20,7 @@ BEBOP_TNG_SFH = "/lcrc/project/halotools/alarcon/results/tng_diffstar_tpeak/"
 LGMH_MIN, LGMH_MAX = 11, 14.75
 N_LGM_BINS = 12
 LOGMH_BINS = np.linspace(LGMH_MIN, LGMH_MAX, N_LGM_BINS)
-LOGMSTAR_BINS_PDF = np.linspace(7, 12, 26)
+LOGMSTAR_BINS_PDF = np.linspace(7, 13, 26)
 LOGSSFR_BINS_PDF = np.linspace(-13, -8, 30)
 
 Z_BINS = [0.0, 0.5, 1.0, 1.5, 2.0]
