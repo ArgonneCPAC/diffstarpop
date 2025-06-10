264c1c3ce99e85618c2f6863bcfcf0f4e7563472
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-06-10 17:46:33 +0200

    Adding SMDPL DR1 initial guess files

--- diffstarpop/kernels/params/__init__.py
+++ diffstarpop/kernels/params/__init__.py
@@ -5,6 +5,7 @@
 import typing
 from collections import namedtuple, OrderedDict
 
+# SMDPL
 from .params_diffstarfits_line_sepms_satfrac_smdpl import (
     DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS as DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS_line_sepms_satfrac,
     DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS as DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac,
@@ -15,9 +16,20 @@ from .params_diffstarfits_line_sepms_satfrac_sigslope_smdpl import (
     DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS as DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac_sigslope,
 )
 
+# SMDPL DR1
+from .params_diffstarfits_line_sepms_satfrac_smdpl_DR1 import (
+    DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS as DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS_line_sepms_satfrac,
+    DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS as DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac,
+)
+
+from .params_diffstarfits_line_sepms_satfrac_sigslope_smdpl_DR1 import (
+    DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS as DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS_line_sepms_satfrac_sigslope,
+    DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS as DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac_sigslope,
+)
+
 sim_name_list = [
     "smdpl",
-    "smdpl_DR1",
+    "smdpl_dr1",
     "tng",
     "galacticus_in_situ",
     "galacticus_in_plus_ex_situ",
@@ -25,15 +37,19 @@ sim_name_list = [
 
 
 DiffstarPop_Params_Diffstarfits_line_sepms_satfrac = OrderedDict(
-    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS_line_sepms_satfrac
+    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS_line_sepms_satfrac,
+    smdpl_dr1=DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS_line_sepms_satfrac,
 )
 DiffstarPop_UParams_Diffstarfits_line_sepms_satfrac = OrderedDict(
-    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac
+    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac,
+    smdpl_dr1=DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac,
 )
 
 DiffstarPop_Params_Diffstarfits_line_sepms_satfrac_sigslope = OrderedDict(
-    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS_line_sepms_satfrac_sigslope
+    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS_line_sepms_satfrac_sigslope,
+    smdpl_dr1=DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS_line_sepms_satfrac_sigslope,
 )
 DiffstarPop_UParams_Diffstarfits_line_sepms_satfrac_sigslope = OrderedDict(
-    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac_sigslope
+    smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac_sigslope,
+    smdpl_dr1=DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS_line_sepms_satfrac_sigslope,
 )
--- diffstarpop/kernels/params/tests/test_params_diffstarfits_line_sepms_satfrac.py
+++ diffstarpop/kernels/params/tests/test_params_diffstarfits_line_sepms_satfrac.py
@@ -11,27 +11,46 @@ from ...defaults_tpeak_line_sepms_satfrac import (
     get_bounded_diffstarpop_params,
 )
 
+# SMDPL
 from ..params_diffstarfits_line_sepms_satfrac_smdpl import (
-    DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS,
-    DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS,
+    DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS as PARAMS_SMDPL,
+    DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS as U_PARAMS_SMDPL,
+)
+
+# SMDPL DR1
+from ..params_diffstarfits_line_sepms_satfrac_smdpl_DR1 import (
+    DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS as PARAMS_SMDPL_DR1,
+    DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS as U_PARAMS_SMDPL_DR1,
 )
 
 
 def test_smdpl():
-    arr_params = tuple_to_array(DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS)
-    arr_u_params = tuple_to_array(DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS)
+    arr_params = tuple_to_array(PARAMS_SMDPL)
+    arr_u_params = tuple_to_array(U_PARAMS_SMDPL)
 
     assert np.all(np.isfinite(arr_params))
     assert np.all(np.isfinite(arr_u_params))
 
-    arr_u_params_bound = get_bounded_diffstarpop_params(
-        DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS
-    )
+    arr_u_params_bound = get_bounded_diffstarpop_params(U_PARAMS_SMDPL)
     arr_u_params_bound = tuple_to_array(arr_u_params_bound)
-    assert np.allclose(arr_params, arr_u_params_bound, rtol=1e-4)
+    assert np.allclose(arr_params, arr_u_params_bound, rtol=1e-4, atol=1e-4)
 
-    arr_params_unbound = get_unbounded_diffstarpop_params(
-        DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS
-    )
+    arr_params_unbound = get_unbounded_diffstarpop_params(PARAMS_SMDPL)
     arr_params_unbound = tuple_to_array(arr_params_unbound)
-    assert np.allclose(arr_u_params, arr_params_unbound, rtol=1e-4)
+    assert np.allclose(arr_u_params, arr_params_unbound, rtol=1e-4, atol=1e-4)
+
+
+def test_smdpl_dr1():
+    arr_params = tuple_to_array(PARAMS_SMDPL_DR1)
+    arr_u_params = tuple_to_array(U_PARAMS_SMDPL_DR1)
+
+    assert np.all(np.isfinite(arr_params))
+    assert np.all(np.isfinite(arr_u_params))
+
+    arr_u_params_bound = get_bounded_diffstarpop_params(U_PARAMS_SMDPL_DR1)
+    arr_u_params_bound = tuple_to_array(arr_u_params_bound)
+    assert np.allclose(arr_params, arr_u_params_bound, rtol=1e-4, atol=1e-4)
+
+    arr_params_unbound = get_unbounded_diffstarpop_params(PARAMS_SMDPL_DR1)
+    arr_params_unbound = tuple_to_array(arr_params_unbound)
+    assert np.allclose(arr_u_params, arr_params_unbound, rtol=1e-4, atol=1e-4)
