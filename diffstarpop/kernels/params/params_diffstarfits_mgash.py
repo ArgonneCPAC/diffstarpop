e2e42ca8f034b1f28c98b951f7ada950b19cb66c
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-09-15 11:26:19 +0200

    adding galacticus diffstarfits initial guesses

--- diffstarpop/kernels/params/params_diffstarfits_mgash.py
+++ diffstarpop/kernels/params/params_diffstarfits_mgash.py
@@ -26,7 +26,7 @@ from .params_diffstarfits_mgash_tng import (
     DIFFSTARFITS_TNG_DIFFSTARPOP_U_PARAMS as DIFFSTARFITS_TNG_DIFFSTARPOP_U_PARAMS_mgash,
 )
 
-"""
+
 # Glacticus in situ
 
 from .params_diffstarfits_mgash_galacticus_in_situ import (
@@ -39,7 +39,7 @@ from .params_diffstarfits_mgash_galacticus_in_plus_ex_situ import (
     DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_PARAMS as DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_PARAMS_mgash,
     DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_U_PARAMS as DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_U_PARAMS_mgash,
 )
-"""
+
 sim_name_list = [
     "smdpl",
     "smdpl_dr1",
@@ -52,13 +52,13 @@ DiffstarPop_Params_Diffstarfits_mgash = OrderedDict(
     # smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_PARAMS_mgash,
     # smdpl_dr1=DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_PARAMS_mgash,
     tng=DIFFSTARFITS_TNG_DIFFSTARPOP_PARAMS_mgash,
-    # galacticus_in_situ=DIFFSTARFITS_GALACTICUS_IN_DIFFSTARPOP_PARAMS_mgash,
-    # galacticus_in_plus_ex_situ=DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_PARAMS_mgash,
+    galacticus_in_situ=DIFFSTARFITS_GALACTICUS_IN_DIFFSTARPOP_PARAMS_mgash,
+    galacticus_in_plus_ex_situ=DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_PARAMS_mgash,
 )
 DiffstarPop_UParams_Diffstarfits_mgash = OrderedDict(
     # smdpl=DIFFSTARFITS_SMDPL_DIFFSTARPOP_U_PARAMS_mgash,
     # smdpl_dr1=DIFFSTARFITS_SMDPL_DR1_DIFFSTARPOP_U_PARAMS_mgash,
     tng=DIFFSTARFITS_TNG_DIFFSTARPOP_U_PARAMS_mgash,
-    # galacticus_in_situ=DIFFSTARFITS_GALACTICUS_IN_DIFFSTARPOP_U_PARAMS_mgash,
-    # galacticus_in_plus_ex_situ=DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_U_PARAMS_mgash,
+    galacticus_in_situ=DIFFSTARFITS_GALACTICUS_IN_DIFFSTARPOP_U_PARAMS_mgash,
+    galacticus_in_plus_ex_situ=DIFFSTARFITS_GALACTICUS_INPLUSEX_DIFFSTARPOP_U_PARAMS_mgash,
 )
